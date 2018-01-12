f06e6b88330c0282a78b5346db909891299568d8
Tu Nombre <you@example.com>
2018-01-12 14:06:46 -0600

    Código completo versión coche sin textura

---
+++ Assets/Script/CameraStable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour {
+
+    //Creamos el objeto Carro 
+    public GameObject TheCar;
+
+    public float CarX;
+    public float CarY;
+    public float CarZ;
+
+
+    // Update is called once per frame
+    void Update () {
+        //Obtenemos las posiciones de X, Y y Z para estabilizar la camara 
+        CarX = TheCar.transform.eulerAngles.x;
+        CarY = TheCar.transform.eulerAngles.y;
+        CarZ = TheCar.transform.eulerAngles.z;
+
+        //Estabilizamos la camara solo obteniendo la posición en Y
+        transform.eulerAngles = new Vector3(CarX - CarX, CarY, CarZ - CarZ);
+    }
+}
